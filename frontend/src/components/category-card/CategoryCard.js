070544cd57aa4545a1c259c2ef96ce890864d601
zhir-aryan <zhiraryan@gmail.com>
2022-04-26 15:12:17 +0300

    desc price on carts

--- frontend/src/components/category-card/CategoryCard.js
+++ frontend/src/components/category-card/CategoryCard.js
@@ -76,24 +76,17 @@ function CategoryCard(props) {
                     ></div>
                     <div className="bottom">
                         <div className="left">
-                            <div className="details">
-                                <h1 style={{ fontSize: '20px' }}>{props.cart.nameKR}</h1>
+                            <div className="details ">
+                                <h1 >{props.cart.nameKR}</h1>
                                 <p>${props.cart.price}</p>
                             </div>
                             {/* <button style={{ width: "200px" }} value="add to card" onClick={() => { localStorage.setItem("cart", JSON.stringify([...cart, props.cart])); setCart([...cart, props.cart]) }} /> */}
                             <div className={`buy ${classes}`} onClick={addToCart}>
                                 {/* className={`buy ${bought ? 'bought' : ''}`} */}
-                                <GrCart style={{ zoom: '220%', margin: '10px 10px' }} />
+                                <GrCart style={{ zoom: '150%' }} />
                             </div>
                         </div>
-                        <div className="right">
-                            {/* <div className="done"><i className="material-icons">done</i></div> */}
-                            <div className="details">
-                                <h1>Chair</h1>
-                                <p>Added to your cart</p>
-                            </div>
-                            {/* <div className="remove"><i className="material-icons">clear</i></div> */}
-                        </div>
+
                     </div>
                 </div>
                 <div className="inside">
